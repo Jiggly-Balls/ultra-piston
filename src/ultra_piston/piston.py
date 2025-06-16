aa78c2d4bd431d9547e11e1f45c98d7bb9f39eb6
Jiggly Balls <jigglyballs9000@gmail.com>
2025-06-17 01:07:54 +0530

    Added all API methods & finished 'get_runtimes'

--- src/ultra_piston/piston.py
+++ src/ultra_piston/piston.py
@@ -2,19 +2,60 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from .request_client import RequestClient
+from . import __title__, __version__
+from .http_clients import HTTPXClient
 
 if TYPE_CHECKING:
-    from typing import Optional, Type, Union
+    from typing import Any, Optional, Type, Union
+
+    from .http_clients import AbstractHTTPClient
 
 
 class PistonClient:
     def __init__(
         self,
         *,
-        base_url: Optional[str],
         api_key: Optional[str] = None,
         rate_limit: Union[float, int] = 1.0,
-        request_client: Type[RequestClient],
+        app_name: str = "Ultra-Piston-Wrapper",
+        base_url: str = "https://emkc.org/api/v2/piston/",
+        http_client: Type[AbstractHTTPClient] = HTTPXClient,
+        **http_client_kwargs: Any,
     ) -> None:
-        self.api_key: Optional[str] = api_key
+        self._http_client: AbstractHTTPClient = http_client()
+
+        if not base_url.endswith("/"):
+            base_url += "/"
+
+        self._http_client.base_url = base_url
+        self._http_client.rate_limit = rate_limit
+        self._http_client.headers = {
+            "User-Agent": f"{self._http_client.driver}; {__title__} v{__version__}; {app_name}",
+            "Content-Type": "application/json",
+        }
+        if api_key:
+            self._http_client.headers["Authorization"] = api_key
+
+        for key, value in http_client_kwargs.items():
+            setattr(self._http_client, key, value)
+
+    def get_runtimes(self) -> dict[str, Any]:
+        return self._http_client.get("runtimes")
+
+    async def get_runtimes_async(self) -> ...: ...
+
+    def get_packages(self) -> ...: ...
+
+    async def get_packages_async(self) -> ...: ...
+
+    def post_packages(self) -> ...: ...
+
+    async def post_packages_async(self) -> ...: ...
+
+    def post_execute(self) -> ...: ...
+
+    async def post_execute_async(self) -> ...: ...
+
+    def delete_packages(self) -> ...: ...
+
+    async def delete_packages_async(self) -> ...: ...
