1f74714e649ffdadcdfe471f7d7e34b97962bbcc
Jiggly Balls <jigglyballs9000@gmail.com>
2025-06-17 01:05:01 +0530

    Added metadata

---
+++ src/ultra_piston/__init__.py
@@ -0,0 +1,9 @@
+from .piston import PistonClient
+
+__all__ = ("PistonClient",)
+
+__version__ = "0.1.0"
+__title__ = "ultra-piston"
+__author__ = "Jiggly-Balls"
+__license__ = "MIT"
+__copyright__ = "Copyright 2025-present Jiggly Balls"
