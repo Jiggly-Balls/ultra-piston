b2fd247d0d166bb18ea9730c3eeb20f8591d18cc
Jiggly Balls <jigglyballs9000@gmail.com>
2025-06-18 19:33:17 +0530

    Added File, RunStage, CompileStage & ExecutionOutput Models

--- src/ultra_piston/models.py
+++ src/ultra_piston/models.py
@@ -1,9 +1,8 @@
-from typing import List, Optional
+from typing import List, Literal, Optional, Union
 
 from pydantic import BaseModel
 
-
-__all__ = ("Runtime", "Package")
+__all__ = ("Runtime", "Package", "File")
 
 
 class Runtime(BaseModel):
@@ -17,3 +16,34 @@ class Package(BaseModel):
     language: str
     language_version: str
     installed: bool
+
+
+class File(BaseModel):
+    name: Optional[str]
+    content: str
+    encoding: Literal["base64", "hex", "utf8"] = "utf8"
+
+
+class RunStage(BaseModel):
+    code: Optional[int]
+    output: str
+    stderr: str
+    stdout: str
+    signal: Optional[str]
+
+
+class CompileStage(BaseModel):
+    code: Optional[int]
+    output: str
+    stderr: str
+    stdout: str
+    signal: Optional[str]
+
+
+class ExecutionOutput(BaseModel):
+    language: str
+    version: str
+    run: RunStage
+    compile: Optional[CompileStage] = None
+    compile_memory_limit: Optional[int] = None
+    compile_timeout: Optional[Union[int, float]] = None
