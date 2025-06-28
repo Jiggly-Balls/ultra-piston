fb649592e961aebd83fd8f5a7b90535be04cb6a3
Jiggly Balls <jigglyballs9000@gmail.com>
2025-06-28 14:06:11 +0530

    Removed ratelimit tests

--- tests/test_piston_async.py
+++ tests/test_piston_async.py
@@ -1,31 +1,11 @@
 from __future__ import annotations
 
-import time
-
 import pytest
 
-from src.ultra_piston import File, HTTPXClient, PistonClient
+from src.ultra_piston import File, PistonClient
 from src.ultra_piston.errors import NotFoundError
 
 pytest_plugins = ("pytest_asyncio",)
-
-
-@pytest.mark.asyncio
-async def test_ratelimit() -> None:
-    class RatelimitTest(HTTPXClient):
-        @HTTPXClient.async_ratelimit
-        async def test_function(self) -> None:
-            return None
-
-    ratelimiter_client = RatelimitTest()
-    ratelimiter_client.rate_limit = 1.0
-
-    start = time.perf_counter()
-    await ratelimiter_client.test_function()
-    await ratelimiter_client.test_function()
-    end = time.perf_counter()
-
-    assert 1 <= (end - start) <= 1.5
 
 
 @pytest.mark.asyncio
--- tests/test_piston_sync.py
+++ tests/test_piston_sync.py
@@ -1,28 +1,9 @@
 from __future__ import annotations
 
-import time
-
 import pytest
 
-from src.ultra_piston import File, HTTPXClient, PistonClient
+from src.ultra_piston import File, PistonClient
 from src.ultra_piston.errors import NotFoundError
-
-
-def test_ratelimit() -> None:
-    class RatelimitTest(HTTPXClient):
-        @HTTPXClient.sync_ratelimit
-        def test_function(self) -> None:
-            return None
-
-    ratelimiter_client = RatelimitTest()
-    ratelimiter_client.rate_limit = 1.0
-
-    start = time.perf_counter()
-    ratelimiter_client.test_function()
-    ratelimiter_client.test_function()
-    end = time.perf_counter()
-
-    assert 1 <= (end - start) <= 1.5
 
 
 def test_endpoint_methods() -> None:
