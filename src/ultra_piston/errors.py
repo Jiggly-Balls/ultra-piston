eb29d44a279ae9a23baa2321426d954eff215e1e
Jiggly Balls <jigglyballs9000@gmail.com>
2025-06-29 20:10:25 +0530

    Added BadRequestError to __all__

--- src/ultra_piston/errors.py
+++ src/ultra_piston/errors.py
@@ -11,6 +11,7 @@ __all__ = (
     "InternalError",
     "ServerError",
     "MissingDataError",
+    "BadRequestError",
     "TooManyRequestsError",
     "InternalServerError",
     "NotFoundError",
